3829f82d19d6131745686f14ec0cf97ba9216073
ivanova anastasija <ivanova.anastasiia@phystech.edu>
2024-02-25 16:34:42 +0300

    move test files

--- scrapper/src/test/java/edu/java/scrapper/GitHubTest.java
+++ scrapper/src/test/java/edu/java/scrapper/GitHubTest.java
@@ -1,4 +1,4 @@
-package edu.java.scrapper.sources_interaction;
+package edu.java.scrapper;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
